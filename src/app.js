d3ae174180addcb510d59eabab6a46b69799d8f5
Larry Kagan <larry@superiocity.com>
2020-06-02 16:31:10 -0400

    Use Hanlderbars for dynamically rendered content

--- src/app.js
+++ src/app.js
@@ -3,7 +3,29 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Larry'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Larry Kagan'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'Test help message'
+    });
+})
 
 app.get('/Weather', (req, res) => {
     res.send({
