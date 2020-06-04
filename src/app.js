7c825a4537ecd313b905ba87cca2188665ea8bf7
Larry Kagan <larry@superiocity.com>
2020-06-04 11:40:36 -0400

    Accept request parameters and add favicon

--- src/app.js
+++ src/app.js
@@ -40,9 +40,29 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/Weather', (req, res) => {
+    if (! req.query.address) {
+        return res.send({
+            error: 'Address is required'
+        });
+    }
+
     res.send({
         forecast: '',
-        location: ''
+        location: '',
+        address: req.query.address
+    });
+});
+
+app.get('/products', (req, res) => {
+    if (! req.query.search) {
+        return res.send({
+            error: 'You must provide a search term',
+        })
+    }
+
+    console.log(req.query);
+    res.send({
+        products: []
     });
 });
 
