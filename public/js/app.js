fce06e113c272e3df9ff6182a664cfbf963c29b3
Larry Kagan <larry@superiocity.com>
2020-06-06 18:20:14 -0400

    Get weather via API to client side

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
 console.log('Client side JS file loaded');
+
+fetch('/weather?address=boston').then(response => {
+    response.json().then(data => {
+        if (data.error) {
+            return console.error(data.error);
+        }
+
+        console.log(`Location: ${data.location} \nWeather: ${data.forecast}`);
+    })
+})
