63bac5e0da246eed0a80f2e15961d1244389e782
Larry Kagan <larry@superiocity.com>
2020-06-08 11:54:56 -0400

    Submit location via form and get weather

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,28 @@
-console.log('Client side JS file loaded');
+console.log("Client side JS file loaded");
 
-fetch('/weather?address=boston').then(response => {
-    response.json().then(data => {
-        if (data.error) {
-            return console.error(data.error);
-        }
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
 
-        console.log(`Location: ${data.location} \nWeather: ${data.forecast}`);
-    })
-})
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    if (! location) {
+        return console.error('Location is required');
+    }
+
+    console.log(location);
+
+    fetch(`/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return console.error(data.error);
+            }
+
+            console.log(
+                `Location: ${data.location} \nWeather: ${data.forecast}`
+            );
+        });
+    });
+});
