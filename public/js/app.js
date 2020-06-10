f08938bc78cde89bb35809f50f7b57bdaf458cad
Larry Kagan <larry@superiocity.com>
2020-06-10 10:39:11 -0400

    Display API request to page

--- public/js/app.js
+++ public/js/app.js
@@ -2,27 +2,29 @@ console.log("Client side JS file loaded");
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const message1 = document.querySelector("#message1");
+const message2 = document.querySelector("#message2");
 
 weatherForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const location = search.value;
 
-    if (! location) {
-        return console.error('Location is required');
-    }
+    message1.textContent = 'loading...';
+    message2.textContent = '';
 
-    console.log(location);
+    if (! location) {
+        return message1.textContent = 'Location is required';
+    }
 
     fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                return console.error(data.error);
+                return message1.textContent = data.error;
             }
 
-            console.log(
-                `Location: ${data.location} \nWeather: ${data.forecast}`
-            );
+            message1.textContent = `Location: ${data.location}`;
+            message2.textContent = `Weather: ${data.forecast}`;
         });
     });
 });
